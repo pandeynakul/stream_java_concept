0cc32cc152469473e393689e7313a1ab6cb8dd87
pandeynakul <pandeynakul.java@gmail.com>
2026-07-01 12:33:25 +0530

    283 with two pts approach

--- src/com/logic/prog/MoveAllZeroes.java
+++ src/com/logic/prog/MoveAllZeroes.java
@@ -10,6 +10,27 @@ public class MoveAllZeroes {
 
         int[] nums = {0, 1, 0, 3, 12};
         moveZeroes(nums);
+        System.out.println();
+        moveZeroes2PtrsApproach(nums);
+
+    }
+
+    private static void moveZeroes2PtrsApproach(int[] nums) {
+
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            //swap it if non -zero with zeroes
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+
+        }
+        for (int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k] + " ");
+        }
 
     }
 
@@ -23,16 +44,16 @@ public class MoveAllZeroes {
             //if the required element are not zero
             if (nums[i] != 0) {
                 //swap it:
-               nums[j++]=nums[i];
+                nums[j++] = nums[i];
             }
 
         }
-        while(j<nums.length){
-            nums[j]=0;
+        while (j < nums.length) {
+            nums[j] = 0;
             j++;
         }
-        for(int k=0;k<nums.length;k++){
-            System.out.print(nums[k]);
+        for (int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k] + " ");
         }
 
     }
